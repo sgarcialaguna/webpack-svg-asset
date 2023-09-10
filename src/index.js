8f0dffda140a4d312578c7fff3e5a19de5cb0893
Santiago Garcia <santiago.garcia@posteo.de>
2023-09-10 17:54:31 +0200

    Example

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import Werder from "./werder.svg";
+
+render(<img src={Werder} />, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /\.svg/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
